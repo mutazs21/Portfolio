1fc9f9da26c31d4b1370f6d837bec872a96cb0ef
mutazs21 <140478630+mutazs21@users.noreply.github.com>
2024-05-20 20:15:11 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        b: '#2563eb',
+        dark: '#0f172a',
+        html: '#E34F26',
+        css: '#1572B6',
+        js: '#F7DF1E',
+        node: '#5FA04E',
+        git: '#F05032',
+        ps: '#31A8FF',
+        ai: '#FF9A00',
+        react:'#61DAFB',
+        tailwindcolor: '#06B6D4',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+}
+
